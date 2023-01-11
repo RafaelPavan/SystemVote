b1e09074b9abc51f61af3fdc7a4f4106e9364c61
Rafael Pavan <rafael.202010386@unilasalle.edu.br>
2023-01-11 17:41:34 -0300

    endpoints dos GETs de associates

---
+++ src/main/java/br/com/nt/voteSystem/service/associate/GetAllAssociateService.java
@@ -0,0 +1,42 @@
+package br.com.nt.voteSystem.service.associate;
+
+import br.com.nt.voteSystem.builder.BaseDtoSuccessBuilder;
+import br.com.nt.voteSystem.dto.associate.GetAssociateDto;
+import br.com.nt.voteSystem.model.associate.AssociateModel;
+import br.com.nt.voteSystem.repository.associate.AssociateRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GetAllAssociateService {
+
+    private final AssociateRepository associateRepository;
+
+    public GetAllAssociateService(AssociateRepository associateRepository) {
+        this.associateRepository = associateRepository;
+    }
+
+    public ResponseEntity execute() {
+        List<AssociateModel> associateModels = associateRepository.findAll();
+        List<GetAssociateDto> listDto = new ArrayList<>();
+
+        if (associateModels.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new BaseDtoSuccessBuilder<>("Não há associados cadastrados no sistema",
+                            HttpStatus.OK).get());
+        }
+
+        for (AssociateModel model: associateModels){
+            GetAssociateDto dto = GetAssociateDto.transform(model);
+            listDto.add(dto);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseDtoSuccessBuilder<>(listDto,
+                HttpStatus.OK).get());
+    }
+}
